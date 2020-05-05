5521f61bbd910fc09b80e938be4e04177070fcff
julianfrancor <julianfrancor@gmail.com>
2020-05-05 21:42:00 +0000

    function that prints all integers of a list, in reverse order.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in my_list[::1]:
-        print("{:d}".format(i))
+    if not my_list:
+        return None
+    else:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
