46fa5c5c8d8dcb05f6efdc0e83aa9eef8ac9218c
julianfrancor <julianfrancor@gmail.com>
2020-05-06 01:47:03 +0000

    function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,13 +5,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a[0:2]
     # if the elements are less than 2, set the empty spaces to zero
     if (len(tuple_a)) < 2:
-        tuple_a = tuple_a + (0, 0)
+        tuple_a += (0, 0)
     # conditional to check the number of elements in the tuple
     if (len(tuple_b)) > 2:
         tuple_b = tuple_b[0:2]
     # if the elements are less than 2, set the empty spaces to zero
     if (len(tuple_b)) < 2:
-        tuple_b = tuple_b + (0, 0)
+        tuple_b += (0, 0)
     # In this, we add inbuilt sum() to perform summation
     # and zip the like indices using zip() and extend
     # logic to both tuples using map().
