26ff491c89fbe710cbc3336ad275599e30c46550
julianfrancor <julianfrancor@gmail.com>
2020-05-18 23:59:55 +0000

    function that raises a type exception.

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError
+
+"""
+Write a function that raises a type exception.
+
+Prototype: def raise_exception():
+You are not allowed to import any module
+"""
