ee1b67e111aba4bcefa5b6f815793ae9cadc994f
julianfrancor <julianfrancor@gmail.com>
2020-05-19 00:03:38 +0000

    function that raises a name exception with a message. raise NameError('C is fun')

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError('C is fun')
+
+
+"""
+Write a function that raises a name exception with a message.
+
+Prototype: def raise_exception_msg(message=""):
+You are not allowed to import any module
+"""
