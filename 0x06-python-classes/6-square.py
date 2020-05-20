2048661d41df2b50d915c61a3de986813a26bfbd
julianfrancor <julianfrancor@gmail.com>
2020-05-20 02:59:12 +0000

    Coordinates of a square, print with tuples

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -110,7 +110,7 @@ class Square:
             print()
             return
 
-        for i in range(0, self.__position[1]):
+        for i in range(self.__position[1]):
             print()
-        for j in range(0, self.__size):
-            print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+        for i in range(self.__size):
+            print((" " * self.__position[0]) + ("#" * self.__size))
