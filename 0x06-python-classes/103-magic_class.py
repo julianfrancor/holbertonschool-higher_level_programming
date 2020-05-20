1218094f701eae1f65698fa14d47c444b5201975
julianfrancor <julianfrancor@gmail.com>
2020-05-20 05:00:40 +0000

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"Defining math"
 import math
 
 
 class MagicClass:
-    """
-
-    """
+    "Defining math"
     def __init__(self, radius):
+        "Defining math"
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
     def area(self):
-        '''comment'''
-        return (self._MagicClass__radius ** 2) * math.pi
+        "Defining math"
+        return self._MagicClass__radius ** 2 * math.pi
 
     def circumference(self):
-        '''comment'''
-        return (2 * math.pi * self._MagicClass__radius)
+        "Defining math"
+        return 2 * math.pi * self._MagicClass__radius
