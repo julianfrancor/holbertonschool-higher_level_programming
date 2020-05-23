91616c88536e341364c7763e73d9e913a6aff47c
julianfrancor <julianfrancor@gmail.com>
2020-05-23 19:40:45 +0000

    fixing function add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,18 +5,9 @@
 
 
 def add_integer(a, b=98):
-    """Function that returns the sum of two intigers
-
-    Args:
-            a (int) first argument must be integer or float
-            b (int) second argument must be integer or float
-
-    Raise error exceptions:
-            TypeError a must be an integer
-            TypeError b must be an integer
-
-    Returns:
-            Returns an integer: the addition of a and b
+    """Args:
+        a (int) first argument must be integer or float
+        b (int) second argument must be integer or float
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
@@ -25,7 +16,7 @@ def add_integer(a, b=98):
     if type(a) is float('inf') or type(b) is float('inf'):
         raise OverflowError("cannot convert float infinity to integer")
     if type(a) is float('NaN') or type(b) is float('NaN'):
-        raise ValueError("cannot convert float NaN to integer")
+        raise OverflowError("cannot convert float infinity to integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
