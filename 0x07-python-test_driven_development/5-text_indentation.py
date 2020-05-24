a0336cdd69192ca3ebaaa599eb24349af384d4e2
julianfrancor <julianfrancor@gmail.com>
2020-05-24 01:00:20 +0000

    text intentation function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,7 +13,7 @@ def text_indentation(text):
     temp = 0
     for i, c in enumerate(text):
         if c in delimiters:
-            print(text[temp: i + 1].strip(), end = "\n\n")
+            print(text[temp: i + 1].strip(), end="\n\n")
             temp = i + 1
         elif i == len(text) - 1:
             print(text[temp: i + 1].strip(), end="")
