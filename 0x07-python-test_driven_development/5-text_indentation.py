58fc78aa0cd916541bab9268ba40875a9627adac
julianfrancor <julianfrancor@gmail.com>
2020-05-24 00:44:25 +0000

    text intentation function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -16,13 +16,13 @@ def text_indentation(text):
     Returns:
             No return value
     """
-    delimiters = [".", "?", ":"]
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    delimiters = [".", "?", ":"]
     temp = 0
     for i, c in enumerate(text):
         if c in delimiters:
-            print(text[temp: i + 1], end="\n\n")
+            print(text[temp: i + 1].strip(), end="\n\n")
             temp = i + 1
         elif i == len(text) - 1:
             print(text[temp: i + 1].strip(), end="")
