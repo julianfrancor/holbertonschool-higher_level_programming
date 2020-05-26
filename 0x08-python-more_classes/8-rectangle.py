6688e5e7e66ace2e4cad4e1c33cc9185bb94b486
julianfrancor <julianfrancor@gmail.com>
2020-05-26 02:36:06 +0000

    Adding a @staticmethod

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -141,11 +141,8 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        area1 = rect_1.__width * rect_1.__height
-        area2 = rect_2.__width * rect_2.__height
-        if area1 > area2:
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        elif area1 < area2:
+        else:
             return rect_2
-        elif area1 == area2:
-            return rect_1
+
