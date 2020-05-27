cefe64484a1464ade4cf28318aef2b2fa99ef51e
julianfrancor <julianfrancor@gmail.com>
2020-05-27 00:25:43 +0000

    __slots__magic

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""class LockedClass"""
 
 
-class LockedClass:
-    __slots__ = 'first_name'
+class LockedClass(object):
+    """docstring"""
+    __slots__ = ['first_name']
+    def __init__(self, first_name):
+        """comments"""
+        self.first_name = first_name
