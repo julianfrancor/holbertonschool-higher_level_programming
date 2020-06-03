ce360440dc3447705fa8b5815f95352fab2aeecb
julianfrancor <julianfrancor@gmail.com>
2020-06-03 16:55:20 +0000

    function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    in the open function we don't have to put explicit the mode to read "r"
+    because by default if you don't put anything it will read.
+    You just have to specify the mode for the write "w"
+
+    f = open('workfile', 'w')
+
+    The first argument is a string containing the filename.
+    The second argument is another string containing a few
+    characters describing the way in which the file will be used.
+    mode can be 'r' when the file will only be read, 'w' for only
+    writing (an existing file with the same name will be erased),
+    and 'a' opens the file for appending; any data written to the
+    file is automatically added to the end. 'r+' opens the file for both reading and writing.
+    The mode argument is optional; 'r' will be assumed if it’s omitted.
+    """
+    with open("my_file_0.txt", mode="r", encoding="UTF8") as file:
+        print(file.read())
