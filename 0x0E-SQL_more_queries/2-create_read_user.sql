246483cbc663cb55b2cfc0f72fa6b007d948720d
julianfrancor <julianfrancor@gmail.com>
2020-07-14 15:54:30 -0500

    script that creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,3 +7,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SET PASSWORD FOR 'user_0d_2'@'localhost' = PASSWORD('user_0d_2_pwd');
 GRANT SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
