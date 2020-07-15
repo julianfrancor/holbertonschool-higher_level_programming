8ea0e4d170fc3cd9540e98fe3bfc74d0752d94bb
julianfrancor <julianfrancor@gmail.com>
2020-07-14 19:42:12 -0500

    FOREIGN KEY, script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+-- a PRIMARY KEY constraint automatically has a UNIQUE constraint defined on it.
+-- Primary keys cannot be NULL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
