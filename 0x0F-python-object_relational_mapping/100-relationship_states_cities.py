3acfc624ab2cba30af8bb0a3c2f364b03ef54129
julianfrancor <julianfrancor@gmail.com>
2020-08-09 18:51:07 -0500

    task 15

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+""" lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+# import library to create the connection with the db
+from sqlalchemy import create_engine
+# Import  library to Traslate from python to SQL language
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    # Cretate engine the starting point for any SQLAlchemy application
+    # The call to create_engine doesn’t actually connect to the database
+    # right away, it just prepares for when it does need to connect
+    # mysql+mysqldb://user:passwd@localhost/my_db
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # This is the function that does the SQL CREATE TABLE...
+    # or ALTER TABLE... commands.
+    # We passed engine (from the code above) to this function to generate
+    # the table(s) that are mapped to objects that inherit from Base.
+    Base.metadata.create_all(engine)
+
+    # Here we are defining a class called Session and an instance of that
+    # class called sesh. The sessionmaker lets us talk to the database in
+    # a controlled environment. This session is bound to our database using
+    # the engine we defined above.
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+
+    # Querying is done using the .query() method on a Session object.
+    # keep in mind the diference btwn filter = SELECT and filter_by = CREATE
+
+
+    instance_state = State()
+    instance_state.name = "California"
+    instance_city = City()
+    instance_city.name = "San Francisco"
+    instance_state.cities.append(instance_city)
+
+    # Add the new elements
+    sesh.add(instance_state)
+    sesh.add(instance_city)
+
+
+    # Save changes
+    sesh.commit()
+    # To close the connection with the db
+    sesh.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+""" lists all State objects from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """class definition of a City
+    inherits from Base (imported from model_state)
+    links to the MySQL table cities
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
