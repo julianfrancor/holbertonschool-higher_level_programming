e75e94c9d600784250ff35b8e30331f17ccab9c2
julianfrancor <julianfrancor@gmail.com>
2020-08-09 00:11:42 -0500

    two tables Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,11 +11,8 @@ class City(Base):
     """class definition of a City
     inherits from Base (imported from model_state)
     links to the MySQL table cities
-    class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-    class attribute name that represents a column of a string of 128 characters and can’t be null
-    class attribute state_id that represents a column of an integer, can’t be null and is a foreign key to states.id
     """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer,ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'))
