ffdc41b57f02256677e593f776c99f0e54084cad
julianfrancor <julianfrancor@gmail.com>
2020-08-18 21:11:56 -0500

    function that find a peak with Timsort python for sorting

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,14 @@
 def find_peak(list_of_integers):
     """Timsort - function that finds a peak in a list of unsorted integers"""
     if len(list_of_integers) == 0:
-        return
-    list_of_integers.sort()
-    return list_of_integers[-1]
+        return "None"
+    lista = list_of_integers.copy()
+    right = len(lista) - 1
+    left = 0
+    while left < right:
+        mid = left + (right - left) // 2
+        if (lista[mid] < lista[mid + 1]):
+            left = mid + 1
+        else:
+            right = mid
+    return lista[left]
