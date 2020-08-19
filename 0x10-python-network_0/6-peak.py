e1e2a2ad5f191bb4bf3a19d9e8e3f2630d8e1802
julianfrancor <julianfrancor@gmail.com>
2020-08-18 22:29:13 -0500

    find peak with python

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,17 +4,8 @@
 
 
 def find_peak(list_of_integers):
-    """Binary Search - function that finds a peak in
-        a list of unsorted integers"""
+    """Timsort - function that finds a peak in a list of unsorted integers"""
     if len(list_of_integers) == 0:
-        return "None"
-    lista = list_of_integers.copy()
-    right = len(lista) - 1
-    left = 0
-    while left < right:
-        mid = left + (right - left) // 2
-        if (lista[mid] < lista[mid + 1]):
-            left = mid + 1
-        else:
-            right = mid
-    return lista[left]
+        return
+    list_of_integers.sort()
+    return list_of_integers[-1]
