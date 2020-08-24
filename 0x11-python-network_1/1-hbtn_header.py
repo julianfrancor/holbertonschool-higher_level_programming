1ea8fef98e39dc856cbf89e79cf869669174801c
julianfrancor <julianfrancor@gmail.com>
2020-08-24 15:18:05 -0500

    using sys and urllib method info(), to display a specific value of the header as a dictionary

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response."""
+import sys
+import urllib.request
+
+
+def function_to_fetch():
+    "Function that fetches a URL"
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        header = response.info()
+        print(header['X-Request-Id])
+
+if __name__ == "__main__":
+    function_to_fetch()
