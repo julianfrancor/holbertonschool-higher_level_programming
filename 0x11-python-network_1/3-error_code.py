3db4649119df569a5af5a33a497efa598940db34
julianfrancor <julianfrancor@gmail.com>
2020-08-24 17:41:23 -0500

    Error code handler, raise an HTTPError and display an error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8)."""
+import sys
+import urllib.request
+
+
+def error_code_handler():
+    "Function that raise an HTTPError"
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print ("Error code:", e.code)
+
+if __name__ == "__main__":
+    error_code_handler()
