76e5cf157d537a1a0c03bbc45dcbdf68e1d334a6
julianfrancor <julianfrancor@gmail.com>
2020-08-24 21:38:02 -0500

    sending a request to github API to get my user ID

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    print("{}".format(r.json()['id']))
