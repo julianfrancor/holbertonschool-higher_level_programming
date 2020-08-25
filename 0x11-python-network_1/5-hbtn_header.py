d28913aa582e9cd39777153670ba375b71761949
julianfrancor <julianfrancor@gmail.com>
2020-08-24 20:01:01 -0500

    displaying content from the headers using requests module

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,6 +4,7 @@ import sys
 import requests
 
 
-url = sys.argv[1]
-r = requests.get(url)
-print(r.headers.get('X-Request-Id', None))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
