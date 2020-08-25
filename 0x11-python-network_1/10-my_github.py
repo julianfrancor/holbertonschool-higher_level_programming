be61fe84a21ab7a315fd25dac8eacca687e58e79
julianfrancor <julianfrancor@gmail.com>
2020-08-24 21:41:19 -0500

    sending a request to github API to get my user ID

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,4 +8,4 @@ import requests
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
     r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
-    print("{}".format(r.json()['id']))
+    print("{}".format(r.json().get('id'))
