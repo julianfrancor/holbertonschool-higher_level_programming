5b392cf43eeeebedd06072ffff82da9feb3a90cc
julianfrancor <julianfrancor@gmail.com>
2020-09-15 19:38:12 -0500

    task 8: using ''.repeat(numberTimes), script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that prints a square
+
+const size = process.argv.slice(2);
+
+if (isNaN(parseInt(size[0], 10))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(size[0], 10); i++) {
+    console.log('X'.repeat(parseInt(size[0])));
+  }
+}
