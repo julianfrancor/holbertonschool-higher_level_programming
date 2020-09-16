3776da0aad853ef8a3420ddf40130a445ac42034
julianfrancor <julianfrancor@gmail.com>
2020-09-15 22:49:31 -0500

    task 11: script that searches the second biggest integer in the list of arguments.

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// script that searches the second biggest integer in the list of arguments.
+
+const list = process.argv.slice(2);
+
+if (list.length === 0) {
+  console.log(0);
+} else {
+  list.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (let i = 0; list.length > i; i++) {
+    list[i] = parseInt(list[i], 10);
+  }
+  console.log(list[list.length - 2]);
+}
