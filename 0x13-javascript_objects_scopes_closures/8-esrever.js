f1bd2de70fe50cf16a08f6efc7038d36139c0cb6
julianfrancor <julianfrancor@gmail.com>
2020-09-21 17:53:26 -0500

    task 8: function that returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,6 +2,9 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  const listReverse = list.reverse();
+  const listReverse = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    listReverse.push(list[i]);
+  }
   return listReverse;
 };
