4f86d6a602c29188f48bf1a842bab749416f4437
julianfrancor <julianfrancor@gmail.com>
2020-09-24 12:57:36 -0500

    task 3: using jQuery API, addClass() on click

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -2,6 +2,6 @@
 // Javascript script that adds the class red to the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#red_header
 // You must use the jQuery API
 // https://www.w3schools.com/jquery/html_addclass.asp
-$('DIV#red_header').click(function(){
+$('DIV#red_header').click(function () {
   $('HEADER').addClass('red');
 });
