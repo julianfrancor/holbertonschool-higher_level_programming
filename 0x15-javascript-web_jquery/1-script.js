e2be23aac74ec0f75b003c6ceab6af72ccd94755
julianfrancor <julianfrancor@gmail.com>
2020-09-24 12:27:01 -0500

    task 1: using jQuery API, script that updates the text color of the HTML tag HEADER to red (#FF0000)

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -2,6 +2,7 @@
 // Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000)
 // You can’t use document.querySelector to select the HTML tag
 // You must use the jQuery API
+// https://jquery-tutorial.net/selectors/using-elements-ids-and-classes/
 $(function()
 {
 	$("HEADER").css("color", "#FF0000");
