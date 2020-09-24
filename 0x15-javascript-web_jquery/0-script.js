bb0551dc7a691a7651a810fa1e5704035bffdd5b
julianfrancor <julianfrancor@gmail.com>
2020-09-24 12:31:02 -0500

    task 0: using document.querySelector to Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000)

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -2,4 +2,5 @@
 // Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000)
 // You must use document.querySelector to select the HTML tag
 // You can’t use the jQuery API
+// https://pbaumgarten.com/javascript/dom.html
 document.querySelector('HEADER').style.color = '#FF0000';
